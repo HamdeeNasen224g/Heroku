cf65cad587534515324ff8d6663e589a3929cce3
Hamdee <75886000+HamdeeNasen224g@users.noreply.github.com>
2021-08-13 14:16:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <h1>Hamdee Naseng 6211273</h1>
-<iframe src="https://thingspeak.com/channels/1458412/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Humidity&type=line">
+<iframe src="https://thingspeak.com/channels/1458412/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Humidity&type=line"></iframe>
 <br>
-<iframe src="https://thingspeak.com/channels/1458412/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Temperature&type=line">
+<iframe src="https://thingspeak.com/channels/1458412/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Temperature&type=line"></iframe>
 <br>
-<iframe src="https://thingspeak.com/channels/1458412/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Light+Value&type=line">
+<iframe src="https://thingspeak.com/channels/1458412/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Light+Value&type=line"></iframe>
 <br>
 
