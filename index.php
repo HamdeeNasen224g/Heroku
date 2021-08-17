2f9edf8fdc8d322a0ac1bdf2c8d2641d8e3387ca
Hamdee <75886000+HamdeeNasen224g@users.noreply.github.com>
2021-08-18 01:33:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,22 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
+    <body onLoad="JavaScript:timedRefresh(3000);"><BR>
+    <?php
+    //ดึง json จาก ais มา 
+$response = file_get_contents("https://magellan.ais.co.th/pullmessageapis/api/listen/thing/59740BBAF7C0BEB8891F1896ADF19ACD", false, stream_context_create($arrContextOptions));
+$json = json_decode($response);
+//เลือกแค่ data
+$thingname = $json->ThingName;
+$temp1 = $json->Sensor->Temperature1;
+$temp2 = $json->Sensor->Temperature2;
+$hum =  $json->Sensor->humidity;
+$tempevm=$json->Sensor->temperatureEVM;
+$d1 = date("y-m-d H:i:s");
+
+//echo $temp1."<br>". $temp2 ."<br>".$hum;
+?>
+    
 <h1>Hamdee Naseng 6211273</h1>
 <iframe src="https://thingspeak.com/channels/1458412/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Humidity&type=line"></iframe>
 <br>
@@ -15,4 +31,5 @@
 <br>
 <iframe src="https://thingspeak.com/channels/1458412/maps/channel_show"></iframe>
 <br>
+        </body>
 </html>
