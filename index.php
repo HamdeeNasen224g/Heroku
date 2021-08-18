a1c4539244cd957e16b2734ead294327e44da969
Hamdee <75886000+HamdeeNasen224g@users.noreply.github.com>
2021-08-18 12:43:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,33 +10,60 @@
 </head>
     <body onLoad="JavaScript:timedRefresh(3000);"><BR>
     <?php
+    $arrContextOptions=array(
+        "ssl"=>array(
+            "verify_peer"=>false,
+            "verify_peer_name"=>false,
+        ),
+    );  
     //ดึง json จาก API 
 $response = file_get_contents("https://api.thingspeak.com/channels/1458412/feeds.json?results=2", false, stream_context_create($arrContextOptions));
 $json = json_decode($response);
 //เลือกแค่ data
 $name = $json->channel->name;
-$hum = $json->feeds;
-$temp = $json->feeds->field2;
+$hum = $json->feeds[1]->field1;
+$temp = $json->feeds[1]->field2;
 
-$d1 = date("y-m-d H:i:s");
-
+$time = $json->feeds[1]->created_at;
+$newtime = date("d-m-Y H:i", strtotime($time)); 
 ?>
     
-<h1>Hamdee Naseng 6211273 <?php echo $hum; ?></h1>
+<h1>Hamdee Naseng 6211273 </h1>
+<br>
+
+    
 <div class="container">
-    <div class="row">
-<iframe class="col-sm-7 " width=100% height=100% src="https://thingspeak.com/channels/1458412/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Humidity&type=line"></iframe>
-</div>
+  <div class="row">
+    <div class="col-sm-7">
+      <h3>Dashboard</h3>
+      <div class="container">
+        <div class="row">
+        <iframe class="col"  src="https://thingspeak.com/channels/1458412/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Humidity&type=line"></iframe>
+        </div>
     <br>
-    <div class="row">
-<iframe class="col-sm-7 " width=100% height=100%  src="https://thingspeak.com/channels/1458412/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Temperature&type=line"></iframe>
-</div>
+        <div class="row">
+        <iframe class="col" src="https://thingspeak.com/channels/1458412/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Temperature&type=line"></iframe>
+        </div>
     <br>
-    <div class="row">
-<iframe class="col-sm-7 " width=100% height=100% src="https://thingspeak.com/channels/1458412/maps/channel_show"></iframe>
-</div>
+        <div class="row">
+        <iframe class="col" src="https://thingspeak.com/channels/1458412/maps/channel_show"></iframe>
+        </div>
     <br>
 </div>
     </div>
+    <div class="col-sm-5">
+      <h3>Data Latest</h3>
+      <div class="container">
+    <h3>Humidity : <?php echo $hum; ?> % <br>
+    Temperature: <?php echo $temp; ?> C <br>
+    Created at: <?php echo $newtime; ?>   <br>
+    </h3>
+</div>
+    </div>
+    
+  </div>
+</div>
+
+
         </body>
 </html>
